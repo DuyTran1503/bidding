d6c1ed23c9b36a0aba7623be092535fd4c3f7413
DuyTran <ngocduytran000315@gmail.com>
2024-07-25 07:17:16 +0700

    update module staff

--- src/services/store/account/account.slice.ts
+++ src/services/store/account/account.slice.ts
@@ -1,13 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 import { EFetchStatus } from "@/shared/enums/fetchStatus";
-import { IInitialState } from "@/shared/utils/shared-interfaces";
+import { IInitialState, IResponse } from "@/shared/utils/shared-interfaces";
 import { commonStaticReducers } from "@/services/shared";
+import { IStaff } from "./account.model";
+import { getAllStaff } from "./account.thunk";
 
-export interface IAccountInitialState extends IInitialState {}
+export interface IAccountInitialState extends IInitialState {
+  staffs: IStaff[];
+}
 
 const initialState: IAccountInitialState = {
   status: EFetchStatus.IDLE,
+  staffs: [],
   message: "",
   filter: {
     page: 1,
@@ -21,6 +26,14 @@ const accountSlice = createSlice({
   initialState,
   reducers: {
     ...commonStaticReducers<IAccountInitialState>(),
+  },
+
+  extraReducers(builder) {
+    builder.addCase(getAllStaff.fulfilled, (state, { payload }: PayloadAction<IResponse<IStaff[]> | any>) => {
+      if (payload.data) {
+        state.staffs = payload.data.data;
+      }
+    });
   },
 });
 export const { setFilter } = accountSlice.actions;
