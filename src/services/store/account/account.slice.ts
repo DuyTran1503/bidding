835f0886031079ab7e773f1055c1863304bb7354
DuyTran <ngocduytran000315@gmail.com>
2024-08-04 15:45:09 +0700

    fix role

--- src/services/store/account/account.slice.ts
+++ src/services/store/account/account.slice.ts
@@ -4,15 +4,17 @@ import { EFetchStatus } from "@/shared/enums/fetchStatus";
 import { IInitialState, IResponse } from "@/shared/utils/shared-interfaces";
 import { commonStaticReducers } from "@/services/shared";
 import { IStaff } from "./account.model";
-import { getAllStaff } from "./account.thunk";
+import { getAllStaff, getStaffById } from "./account.thunk";
 
 export interface IAccountInitialState extends IInitialState {
   staffs: IStaff[];
+  staff?: IStaff | any;
 }
 
 const initialState: IAccountInitialState = {
   status: EFetchStatus.IDLE,
   staffs: [],
+  staff: undefined,
   message: "",
   filter: {
     page: 1,
@@ -32,9 +34,14 @@ const accountSlice = createSlice({
     builder.addCase(getAllStaff.fulfilled, (state, { payload }: PayloadAction<IResponse<IStaff[]> | any>) => {
       if (payload.data) {
         state.staffs = payload.data.data;
+        state.totalRecords = payload?.data?.total_elements;
       }
+    });
+    builder.addCase(getStaffById.fulfilled, (state, { payload }: PayloadAction<IStaff> | any) => {
+      state.staff = { ...payload?.staff, list_role: payload?.list_role };
+      state.loading = false;
     });
   },
 });
-export const { setFilter } = accountSlice.actions;
+export const { setFilter, resetStatus } = accountSlice.actions;
 export { accountSlice };
--- src/shared/enums/typeStaff.ts
+++ src/shared/enums/typeStaff.ts
@@ -1,6 +1,7 @@
 export enum ETYPESTAFF {
   ADMIN = "admin",
   SUPER_ADMIN = "super_admin",
+  STAFF = "staff",
   STAFF_1 = "chuyen_vien_1",
   STAFF_2 = "chuyen_vien_2",
 }
