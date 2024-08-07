46764af5b4fd6182baabd28b4c5c10a04d34c0c2
DuyTran <ngocduytran000315@gmail.com>
2024-08-07 10:42:30 +0700

    crud loạt hình hoạt động

--- src/components/table/SearchComponent.tsx
+++ src/components/table/SearchComponent.tsx
@@ -59,104 +59,104 @@ const SearchComponent = <T extends ISearchParams>(props: ISearchProps<T>) => {
         return (
           <div className={`${isShow ? "hidden" : ""} row-gap-3 flex flex-col px-4 py-3`}>
             <Row className="row-gap-2 row-gap-lg-3">
-              {search?.map((item, index) => {
-                if (item.type === "text") {
-                  // @ts-ignore
-                  const value: any = values && [item.id] ? values[item.id] : "";
-
-                  return (
-                    <Col xs={12} sm={12} md={6} lg={4}>
-                      <FormInput
-                        id={item.id}
-                        // icon={IoSearchOutline}
-                        type="text"
-                        label={item.label}
-                        value={value ?? ""}
-                        // error={errors[item.id]}
-                        placeholder="Nhập ..."
-                        onChange={(data) => {
-
-                          setFieldValue(item.id, data).then();
-                          item.onChange && item.onChange(String(data));
-                        }}
-                        onBlur={handleBlur}
-                      />
-                    </Col>
-                  );
-                }
-                if (item.type === "select") {
-                  // @ts-ignore
-                  const value =
+              {search.length &&
+                search?.map((item, index) => {
+                  if (item.type === "text") {
                     // @ts-ignore
-                    values && values[item.id]
-                      ? // @ts-ignore
-                        item.options?.find((option) => option.value === values[item.id])
-                      : null;
-                  const options = item.parentItem ? (values[item.parentItem] ? item.options : []) : item.options;
-                  return (
-                    <Col key={index} xs={12}>
-                      <FormSelect
-                        // isClearable={item?.isClearable ?? true}
-                        id={item.id!}
-                        label={item.title}
-                        placeholder={item.placeholder}
-                        // value={value}
-                        options={options!}
-                        onChange={(data) => {
-                          if (Array.isArray(data)) return;
+                    const value: any = values && [item.id] ? values[item.id] : "";
 
-                          // reset child value
-                          item.childItems?.forEach((child) => {
-                            setFieldValue(child, "").then(() => {});
-                          });
+                    return (
+                      <Col xs={12} sm={12} md={6} lg={4} key={index}>
+                        <FormInput
+                          id={item.id}
+                          // icon={IoSearchOutline}
+                          type="text"
+                          label={item.label}
+                          value={value ?? ""}
+                          // error={errors[item.id]}
+                          placeholder="Nhập ..."
+                          onChange={(data) => {
+                            setFieldValue(item.id, data).then();
+                            item.onChange && item.onChange(String(data));
+                          }}
+                          onBlur={handleBlur}
+                        />
+                      </Col>
+                    );
+                  }
+                  if (item.type === "select") {
+                    // @ts-ignore
+                    const value =
+                      // @ts-ignore
+                      values && values[item.id]
+                        ? // @ts-ignore
+                          item.options?.find((option) => option.value === values[item.id])
+                        : null;
+                    const options = item.parentItem ? (values[item.parentItem] ? item.options : []) : item.options;
+                    return (
+                      <Col key={index} xs={12}>
+                        <FormSelect
+                          // isClearable={item?.isClearable ?? true}
+                          id={item.id!}
+                          label={item.title}
+                          placeholder={item.placeholder}
+                          // value={value}
+                          options={options!}
+                          onChange={(data) => {
+                            if (Array.isArray(data)) return;
 
-                          // set value
-                          setFieldValue(item.id, data).then(() => {});
-
-                          if (!data && item?.valueDefault) {
-                            item.onChange && item.onChange(data ?? "");
-                            return;
-                          }
-
-                          if (data && data) {
-                            item.onChange && item.onChange(data);
-                            return;
-                          }
-                        }}
-                      />
-                    </Col>
-                  );
-                }
-                if (item.type === "datetime") {
-                  // @ts-ignore
-                  const value = values && values[item.id] ? values[item.id] : null;
-
-                  return (
-                    <Col key={index} xs={12}>
-                      <FormDate
-                        id={item.id}
-                        label={item.title}
-                        minDate={item.minDate ? dayjs(item.minDate) : undefined}
-                        // @ts-ignore
-                        value={value ? dayjs(value) : null}
-                        maxDate={item.maxDate ? dayjs(item.maxDate) : undefined}
-                        onChange={(data) => {
-                          if (item?.childItems && item?.childItems.length > 0) {
+                            // reset child value
                             item.childItems?.forEach((child) => {
                               setFieldValue(child, "").then(() => {});
                             });
-                          }
-                          const result = data ? data.format("YYYY-MM-DD") : null;
 
-                          setFieldValue(item.id, result).then();
-                          item.onChange && item.onChange(result as any);
-                        }}
-                      />
-                    </Col>
-                  );
-                }
-                return <React.Fragment key={index} />;
-              })}
+                            // set value
+                            setFieldValue(item.id, data).then(() => {});
+
+                            if (!data && item?.valueDefault) {
+                              item.onChange && item.onChange(data ?? "");
+                              return;
+                            }
+
+                            if (data && data) {
+                              item.onChange && item.onChange(data);
+                              return;
+                            }
+                          }}
+                        />
+                      </Col>
+                    );
+                  }
+                  if (item.type === "datetime") {
+                    // @ts-ignore
+                    const value = values && values[item.id] ? values[item.id] : null;
+
+                    return (
+                      <Col key={index} xs={12}>
+                        <FormDate
+                          id={item.id}
+                          label={item.title}
+                          minDate={item.minDate ? dayjs(item.minDate) : undefined}
+                          // @ts-ignore
+                          value={value ? dayjs(value) : null}
+                          maxDate={item.maxDate ? dayjs(item.maxDate) : undefined}
+                          onChange={(data) => {
+                            if (item?.childItems && item?.childItems.length > 0) {
+                              item.childItems?.forEach((child) => {
+                                setFieldValue(child, "").then(() => {});
+                              });
+                            }
+                            const result = data ? data.format("YYYY-MM-DD") : null;
+
+                            setFieldValue(item.id, result).then();
+                            item.onChange && item.onChange(result as any);
+                          }}
+                        />
+                      </Col>
+                    );
+                  }
+                  return <React.Fragment key={index} />;
+                })}
             </Row>
             <div className="mt-[12px] flex flex-row items-center justify-center gap-2">
               <Button text={"Tìm kiếm"} onClick={handleSubmit} />
