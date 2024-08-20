f5f8b35b428fb1755af8a9a88b2e6979ba49db5a
Dang1901 <138227152+Dang1901@users.noreply.github.com>
2024-08-20 13:39:41 +0700

    update code funding-source create, update, deatail 20/8

--- src/pages/FundingSource/ActionModule.tsx
+++ src/pages/FundingSource/ActionModule.tsx
@@ -4,7 +4,7 @@ import { useArchive } from "@/hooks/useArchive";
 import { IFundingSourceInitialState, resetMessageError } from "@/services/store/funding_source/funding_source.slice";
 import { createFundingSource, updateFundingSource } from "@/services/store/funding_source/funding_source.thunk";
 import { FormikRefType } from "@/shared/utils/shared-types";
-import FormSwitch from "@/components/form/FormSwitch"
+import FormSwitch from "@/components/form/FormSwitch";
 import FormInputArea from "@/components/form/FormInputArea";
 import { Formik } from "formik";
 import { object, string } from "yup";
@@ -12,103 +12,128 @@ import lodash from "lodash";
 import { Col, Row } from "antd";
 import { useEffect } from "react";
 import { EPageTypes } from "@/shared/enums/page";
-import { Form } from "react-router-dom";
 
 interface IFundingSourceFormProps {
-    formikRef?: FormikRefType<IFundingSourceFormInitialValues>;
-    type: EPageTypes.CREATE | EPageTypes.UPDATE | EPageTypes.VIEW
-    fundingSource? : IFundingSourceFormInitialValues
+  formikRef?: FormikRefType<IFundingSourceInitialValues>;
+  type: EPageTypes.CREATE | EPageTypes.UPDATE | EPageTypes.VIEW;
+  fundingSource?: IFundingSourceInitialValues;
 }
 
-export interface IFundingSourceFormInitialValues {
-    id?: string ;
-    name: string;
-    type: string;
-    code: string;
-    description: string;
-    is_active: string;
+export interface IFundingSourceInitialValues {
+  id?: string;
+  name: string;
+  type: string;
+  code: string;
+  description: string;
+  is_active: string;
 }
 
-const FundingSourceForm = ({ formikRef, type, fundingSource }: IFundingSourceFormProps ) => {
-    const { dispatch } = useArchive<IFundingSourceInitialState>("funding_source");
+const FundingSourceForm = ({ formikRef, type, fundingSource }: IFundingSourceFormProps) => {
+  const { dispatch } = useArchive<IFundingSourceInitialState>("funding_source");
 
-    const initialValues: IFundingSourceFormInitialValues = {
-        id: fundingSource?.id ?? "", // kieu du lieu bat buoc
-        name: fundingSource?.name ?? "",
-        type: fundingSource?.type ?? "",
-        code: fundingSource?.code ?? "",
-        description: fundingSource?.description ?? "",
-        is_active: fundingSource?.is_active ?? "",
-    };
-    
-    const tagSchema = object().shape({
-      name: string().trim().required("Vui lòng không để trống trường này"),
-    });
-    useEffect(() => {
+  const initialValues: IFundingSourceInitialValues = {
+    id: fundingSource?.id ?? "", // kieu du lieu bat buoc
+    name: fundingSource?.name ?? "",
+    type: fundingSource?.type ?? "",
+    code: fundingSource?.code ?? "",
+    description: fundingSource?.description ?? "",
+    is_active: fundingSource?.is_active ?? "",
+  };
+
+  const tagSchema = object().shape({
+    name: string().trim().required("Vui lòng không để trống trường này"),
+  });
+  useEffect(() => {
     return () => {
       dispatch(resetMessageError());
     };
   }, []);
 
-    return (
-        <Formik
-        innerRef={formikRef}
-        initialValues={initialValues}
-        validationSchema={tagSchema}
-        onSubmit={(data) => {
-          if (type === EPageTypes.CREATE) {
-            dispatch(createFundingSource({ body: lodash.omit(data, "id") }));
-          } else if (type === EPageTypes.UPDATE && fundingSource?.id) {
-            dispatch(updateFundingSource({ body: lodash.omit(data, "id"), param: fundingSource.id }));
-          }
-        }}
-      >
-        {({ values, errors, touched, handleBlur, setFieldValue }) => (
-          <>
-         
-                    <Row gutter={[24, 24]}>
-                      <Col xs={24} sm={24} md={12} xl={12} className="mb-4">
-                        <FormGroup title="Tên nguồn tài trợ">
-                          <FormInput
-                            label="Tên nguồn tài trợ"
-                            placeholder="Tên nguồn tài trợ..."
-                            name="name"
-                            value={values.name}
-                            error={touched.name ? errors.name : ""}
-                            onChange={(e) => setFieldValue("name", e)}
-                            onBlur={handleBlur}
-                          />
-                        </FormGroup>
-                      </Col>
-                      <Col xs={24} sm={24} md={12} xl={12} className="mb-4">
-                        <FormGroup title="Trạng thái hoạt động">
-                          <FormSwitch
-                            checked={!!values.is_active ? true : false}
-                            onChange={(value) => {
-                              setFieldValue("is_active", value);
-                            }}
-                          />
-                        </FormGroup>
-                      </Col>
-                    </Row>
-                    <Row gutter={[24, 24]}>
-                      <Col xs={24} sm={24} md={24} xl={24} className="mb-4">
-                        <FormGroup title="Mô tả">
-                          <FormInputArea
-                            label="Mô tả"
-                            placeholder="Nhập mô tả..."
-                            name="description"
-                            value={values.description}
-                            error={touched.description ? errors.description : ""}
-                            onChange={(e) => setFieldValue("description", e)}
-                          />
-                        </FormGroup>
-                      </Col>
-                    </Row>
-          </>
-        )}
-      </Formik>
-    );
+  return (
+    <Formik
+      innerRef={formikRef}
+      initialValues={initialValues}
+      validationSchema={tagSchema}
+      onSubmit={(data) => {
+        if (type === EPageTypes.CREATE) {
+          dispatch(createFundingSource({ body: lodash.omit(data, "id") }));
+        } else if (type === EPageTypes.UPDATE && fundingSource?.id) {
+          dispatch(updateFundingSource({ body: lodash.omit(data, "id"), param: fundingSource.id }));
+        }
+      }}
+    >
+      {({ values, errors, touched, handleBlur, setFieldValue }) => (
+        <>
+          <Row gutter={[24, 24]}>
+            <Col xs={24} sm={24} md={12} xl={12} className="mb-4">
+              <FormGroup title="Tên nguồn tài trợ">
+                <FormInput
+                  label="Tên nguồn tài trợ"
+                  placeholder="Tên nguồn tài trợ..."
+                  name="name"
+                  value={values.name}
+                  error={touched.name ? errors.name : ""}
+                  onChange={(e) => setFieldValue("name", e)}
+                  onBlur={handleBlur}
+                />
+              </FormGroup>
+            </Col>
+            <Col xs={24} sm={24} md={12} xl={12} className="mb-4">
+              <FormGroup title="Loại nguồn tài trợ">
+                <FormInput
+                  label="Loại nguồn tài trợ"
+                  placeholder="Loại nguồn tài trợ..."
+                  name="type"
+                  value={values.type}
+                  error={touched.type ? errors.type : ""}
+                  onChange={(e) => setFieldValue("type", e)}
+                  onBlur={handleBlur}
+                />
+              </FormGroup>
+            </Col>
+            <Col xs={24} sm={24} md={12} xl={12} className="mb-4">
+              <FormGroup title="Mã nguồn tài trợ">
+                <FormInput
+                  label="Mã nguồn tài trợ"
+                  placeholder="Mã nguồn tài trợ..."
+                  name="code"
+                  value={values.code}
+                  error={touched.code ? errors.code : ""}
+                  onChange={(e) => setFieldValue("code", e)}
+                  onBlur={handleBlur}
+                />
+              </FormGroup>
+            </Col>
+            <Col xs={24} sm={24} md={12} xl={12} className="mb-4">
+              <FormGroup title="Trạng thái hoạt động">
+                <FormSwitch
+                  checked={!!values.is_active ? true : false}
+                  onChange={(value) => {
+                    setFieldValue("is_active", value);
+                  }}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row gutter={[24, 24]}>
+            <Col xs={24} sm={24} md={24} xl={24} className="mb-4">
+              <FormGroup title="Mô tả">
+                <FormInputArea
+                  label="Mô tả"
+                  placeholder="Nhập mô tả..."
+                  name="description"
+                  isReadonly={type === EPageTypes.VIEW}
+                  value={values.description}
+                  error={touched.description ? errors.description : ""}
+                  onChange={(e) => setFieldValue("description", e)}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+        </>
+      )}
+    </Formik>
+  );
 };
 
 export default FundingSourceForm;
