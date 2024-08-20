6965b3b0090465af523d38e497af5c28627f8419
Dang1901 <138227152+Dang1901@users.noreply.github.com>
2024-08-20 13:39:41 +0700

    update code funding-source create, update, deatail 20/8

--- src/pages/Industry/Detail/index.tsx
+++ src/pages/Industry/Detail/index.tsx
@@ -1,16 +1,15 @@
-import { useEffect, useRef, useState } from "react";
-import { FaPlus } from "react-icons/fa";
-import { IoClose } from "react-icons/io5";
-import { useNavigate, useParams } from "react-router-dom";
-import { FormikProps } from "formik";
 import Heading from "@/components/layout/Heading";
-import { EFetchStatus } from "@/shared/enums/fetchStatus";
-import useFetchStatus from "@/hooks/useFetchStatus";
 import { useArchive } from "@/hooks/useArchive";
-import { EPageTypes } from "@/shared/enums/page";
-import IndustryForm, { IndustryInitialValues } from "../ActionModule";
+import useFetchStatus from "@/hooks/useFetchStatus";
+import IndustryForm from "@/pages/Industry/ActionModule";
 import { IIndustryInitialState, resetStatus } from "@/services/store/industry/industry.slice";
 import { getIndustryById } from "@/services/store/industry/industry.thunk";
+import { EPageTypes } from "@/shared/enums/page";
+import { FormikProps } from "formik";
+import { useEffect, useRef, useState } from "react";
+import { IoClose } from "react-icons/io5";
+import { useNavigate, useParams } from "react-router-dom";
+import { IndustryInitialValues } from "../ActionModule";
 const DetailIndustry = () => {
   const navigate = useNavigate();
   const formikRef = useRef<FormikProps<IndustryInitialValues>>(null);
