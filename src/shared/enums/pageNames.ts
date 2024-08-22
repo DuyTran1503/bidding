1b5e19107ec9d1ee8c06fbfa92e1f1f50e72d823
Dang1901 <138227152+Dang1901@users.noreply.github.com>
2024-08-22 20:43:26 +0700

    update bidbond

--- src/shared/enums/pageNames.ts
+++ src/shared/enums/pageNames.ts
@@ -11,6 +11,7 @@ export enum EPageNames {
   enterprise = "Doanh nghiệp",
   staffs = "Nhân viên",
   "funding-sources" = "Nguồn tài trợ",
+  bidbond = "Bão lãnh dự thầu",
   business_activity = "Loại hình hoạt động",
   bidding_fields = "Lĩnh vực đấu thầu",
   bidding_types = "Loại hình đấu thầu",
