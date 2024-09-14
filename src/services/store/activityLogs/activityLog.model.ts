eb7789ee05f0be5a8967e14c7dbf1965b7351252
DuyTran <ngocduytran000315@gmail.com>
2024-09-14 15:14:38 +0700

    update code

--- src/services/store/activityLogs/activityLog.model.ts
+++ src/services/store/activityLogs/activityLog.model.ts
@@ -1,9 +1,9 @@
-import { ETYPEACTIVITYLOG } from "@/shared/enums/typeActivityLog";
+import { E_TYPE_ACTIVITY } from "@/shared/enums/typeActivityLog";
 
 export interface IActivityLog {
-    id: number | string;
-    log_name: string;
-    event?: ETYPEACTIVITYLOG;
-    action_performer: string;
-    description: string;
-  }
+  id: number | string;
+  log_name: string;
+  event?: E_TYPE_ACTIVITY;
+  action_performer: string;
+  description: string;
+}
