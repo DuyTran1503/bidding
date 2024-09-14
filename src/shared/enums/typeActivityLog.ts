a2dc139ae87ccac9976521d9ba95d3c7b022db0b
DuyTran <ngocduytran000315@gmail.com>
2024-09-14 15:14:38 +0700

    update code

--- src/shared/enums/typeActivityLog.ts
+++ src/shared/enums/typeActivityLog.ts
@@ -1,12 +1,5 @@
-
-export enum ETYPEACTIVITYLOG {
+export enum E_TYPE_ACTIVITY {
   CREATE = "Tạo mới",
   UPDATE = "Cập nhật",
   DESTROY = "Xóa",
 }
-
-export const activityLogOptions = [
-  { value: "CREATE", label: "Tạo mới" },
-  { value: "UPDATE", label: "Cập nhật" },
-  { value: "DESTROY", label: "Xóa" },
-];
