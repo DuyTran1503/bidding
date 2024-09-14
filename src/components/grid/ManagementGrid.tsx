1a560378b2d4858b0842796d380579f35173522b
DuyTran <ngocduytran000315@gmail.com>
2024-09-14 13:56:31 +0700

    update code

--- src/components/grid/ManagementGrid.tsx
+++ src/components/grid/ManagementGrid.tsx
@@ -1,13 +1,21 @@
 import { ColumnsType } from "antd/es/table";
 import PrimaryTable, { ITableData, ScrollProps } from "../table/PrimaryTable";
 import { IGridButton, ISearchParams } from "@/shared/utils/shared-interfaces";
-import { useMemo } from "react";
+import { SetStateAction, useMemo, Dispatch, ReactNode, useState } from "react";
 import { EButtonTypes } from "@/shared/enums/button";
 import GridButtons from "./GridButtons";
 import { TableColumnsType } from "antd";
 import { ActionCreatorWithPayload } from "@reduxjs/toolkit";
 import { ISearchTypeTable } from "../table/SearchComponent";
 
+export interface IModalProps<T> {
+  visible: boolean;
+  setVisible: Dispatch<SetStateAction<boolean>>;
+  item?: T | ITableData;
+  isDetail?: boolean;
+  setItem?: Dispatch<SetStateAction<T | undefined>>;
+  type?: EButtonTypes;
+}
 export interface IGridProps<T extends ISearchParams> {
   columns: ColumnsType;
   data: ITableData[];
@@ -24,6 +32,7 @@ export interface IGridProps<T extends ISearchParams> {
   fetching?: Function;
   filter?: T;
   scroll?: ScrollProps;
+  ModalContent?: (props: IModalProps<T>) => ReactNode;
 }
 
 const ManagementGrid = <T extends ISearchParams>({
@@ -36,7 +45,22 @@ const ManagementGrid = <T extends ISearchParams>({
   fetching,
   filter,
   scroll,
+  ModalContent,
 }: IGridProps<T>) => {
+  const [modalUpdate, setModalUpdate] = useState(false);
+  const [isDetail, setIsDetail] = useState(false);
+  const [item, setItem] = useState<ITableData>({} as ITableData);
+  const [type, setType] = useState<EButtonTypes>(EButtonTypes.VIEW);
+  const handleButtonClick = (record: ITableData, type: EButtonTypes) => {
+    if (type === EButtonTypes.UPDATE || type === EButtonTypes.VIEW) {
+      setItem(record); // Wrap record in an array
+      setIsDetail(type === EButtonTypes.VIEW);
+      setType(type);
+      setModalUpdate(true);
+    } else if (type === EButtonTypes.DESTROY) {
+      // Handle delete action if needed
+    }
+  };
   const renderColumns = useMemo(() => {
     return buttons?.some((button) => button.type === EButtonTypes.VIEW || button.type === EButtonTypes.UPDATE || button.type === EButtonTypes.DESTROY)
       ? ([
@@ -49,23 +73,40 @@ const ManagementGrid = <T extends ISearchParams>({
             fixed: "right",
             align: "center",
             render(_, record) {
-              return <GridButtons buttons={buttons} record={record} />;
+              return <GridButtons buttons={buttons} record={record as any} onClick={(record, type) => handleButtonClick(record as any, type)} />;
             },
           },
         ] as TableColumnsType)
       : columns;
   }, [JSON.stringify(buttons)]);
+  const Modal = useMemo(
+    () =>
+      ModalContent && (
+        <ModalContent
+          type={type}
+          visible={modalUpdate}
+          setVisible={setModalUpdate}
+          item={item} // Pass item, which is now an array
+          isDetail={isDetail}
+          setItem={setItem as any}
+        />
+      ),
+    [modalUpdate, item, isDetail],
+  );
   return (
-    <PrimaryTable
-      scroll={scroll}
-      search={search!}
-      columns={renderColumns}
-      data={data}
-      pagination={pagination}
-      setFilter={setFilter}
-      fetching={fetching}
-      filter={filter!}
-    />
+    <>
+      <PrimaryTable
+        scroll={scroll}
+        search={search!}
+        columns={renderColumns}
+        data={data}
+        pagination={pagination}
+        setFilter={setFilter}
+        fetching={fetching}
+        filter={filter!}
+      />
+      {Modal}
+    </>
   );
 };
 
