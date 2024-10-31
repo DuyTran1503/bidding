33946239d60ee2c8e325d0dd773d24f2c49aa6cf
DuyTran <ngocduytran000315@gmail.com>
2024-10-31 21:42:28 +0700

    update code

--- src/services/store/CompareProject/compareProject.thunk.ts
+++ src/services/store/CompareProject/compareProject.thunk.ts
@@ -2,20 +2,16 @@
 import { client } from "@/services/config/client";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { ICompareProject } from "./compareProject.model";
+import { IThunkPayload } from "@/shared/utils/shared-interfaces";
 
 const prefix = "/api/admin/compare-projects";
 
 // compareprojectt project-by-industry
-export const compareBarChartTotalAmount = createAsyncThunk(
-  "compare-bidder-count",
-  async (payload: ICompareProject, { rejectWithValue }) => {
-    try {
-
-      console.log("Response data after sending:", payload);
-      const { data } = await client.post(`${prefix}/compare-bidder-count`, payload);
-      return data.data;
-    } catch (error: any) {
-      return rejectWithValue(error.response?.data || "Có lỗi xảy ra khi gọi API");
-    }
+export const compareBarChartTotalAmount = createAsyncThunk("compare-bidder-count", async (payload: IThunkPayload, { rejectWithValue }) => {
+  try {
+    const { data } = await client.post(`${prefix}/compare-bidder-count`, payload);
+    return data.data;
+  } catch (error: any) {
+    return rejectWithValue(error.response?.data || "Có lỗi xảy ra khi gọi API");
   }
-);
+});
