f71f08aae0a7d2fa15ad1033e9b730224ce81b4b
DuyTran <ngocduytran000315@gmail.com>
2024-10-31 21:42:28 +0700

    update code

--- src/pages/Project/Statistical/index.tsx
+++ src/pages/Project/Statistical/index.tsx
@@ -17,151 +17,133 @@ import { ICompareProjectInitialState, resetStatus } from "@/services/store/Compa
 import { employeeEducationLevelStatisticByEnterprise, projectByFundingsource, projectByIndustry } from "@/services/store/chart/chart.thunk";
 
 const Statistical: React.FC = () => {
-    const { state: stateChart, dispatch: dispatchChart } = useArchive<IChartInitialState>("chart");
-    const { state: stateProject, dispatch: dispatchProject } = useArchive<IProjectInitialState>("project");
-    const { state: stateCompare, dispatch: dispatchCompare } = useArchive<ICompareProjectInitialState>("compareproject");
-    const [treeData, setTreeData] = useState<{ title: string; value: string; key: string; children?: any[] }[]>([]);
-    const [selectedProjectIds, setSelectedProjectIds] = useState<number[]>([]);
-    const [treeSelectIds, setTreeSelectIds] = useState<string[]>([]);
-    const navigate = useNavigate();
-    const { id } = useParams();
+  const { state: stateChart, dispatch: dispatchChart } = useArchive<IChartInitialState>("chart");
+  const { state: stateProject, dispatch: dispatchProject } = useArchive<IProjectInitialState>("project");
+  const { state: stateCompare, dispatch: dispatchCompare } = useArchive<ICompareProjectInitialState>("compareproject");
+  const [treeData, setTreeData] = useState<{ title: string; value: string; key: string; children?: any[] }[]>([]);
+  const [selectedProjectIds, setSelectedProjectIds] = useState<number[]>([]);
+  const [treeSelectIds, setTreeSelectIds] = useState<string[]>([]);
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-    useFetchStatus({
-        module: "compareproject",
-        reset: resetStatus,
-        actions: {
-            success: {
-                message: stateCompare.message,
+  useFetchStatus({
+    module: "compareproject",
+    reset: resetStatus,
+    actions: {
+      success: {
+        message: stateCompare.message,
+      },
+      error: {
+        message: stateCompare.message,
+      },
+    },
+  });
+
+  useEffect(() => {
+    dispatchChart(projectByIndustry({}));
+    dispatchChart(projectByFundingsource({}));
+    dispatchProject(getListProject())
+      .then(unwrapResult)
+      .then((result) => {
+        const fields = result.data;
+        const formattedData = formatTreeData(fields);
+        setTreeData(formattedData);
+      });
+  }, [dispatchProject]);
+
+  useEffect(() => {
+    if (id) {
+      dispatchProject(getProjectById(id));
+    }
+  }, [id, dispatchProject]);
+
+  useEffect(() => {
+    if (selectedProjectIds.length > 0) {
+      const requestData = {
+        project_ids: selectedProjectIds,
+      };
+      dispatchCompare(compareBarChartTotalAmount({ body: requestData }));
+    }
+  }, [selectedProjectIds, dispatchCompare]);
+
+  useEffect(() => {
+    const investorId = stateProject.project?.investor?.id;
+    if (investorId) {
+      dispatchChart(employeeEducationLevelStatisticByEnterprise(investorId));
+    }
+  }, [stateProject.project, dispatchChart]);
+
+  const handleAddToCompare = () => {
+    const newIds = treeSelectIds.map(Number); // Chuyển đổi chuỗi thành số
+    const investorId = stateProject.project?.investor?.id;
+
+    // Tạo mảng project_ids và thêm investorId nếu có
+    const updatedProjectIds = [...new Set([...selectedProjectIds, ...newIds])]; // Kết hợp và loại bỏ trùng lặp
+
+    if (investorId) {
+      updatedProjectIds.push(investorId); // Thêm investorId vào mảng
+    }
+
+    setSelectedProjectIds(updatedProjectIds);
+  };
+
+  const educationData = stateChart.employeeEducationLevelStatisticByEnterprise || {};
+  const names = Object.keys(educationData);
+  const values = Object.values(educationData).map((value) => Number(value));
+
+  const formatTreeData = (data: any[]): { title: string; value: string; key: string; children?: any[] }[] => {
+    return data.map((item) => ({
+      title: item.name,
+      value: item.id.toString(),
+      key: item.id.toString(),
+      children: item.children ? formatTreeData(item.children) : [],
+    }));
+  };
+
+  const tabItems = [
+    {
+      key: "1",
+      label: "Thống kê dự án",
+      content: (
+        <GenericChart
+          chartType="bar"
+          title="Thống kê ngành"
+          name={stateChart.industryData.map(({ name }) => name)}
+          value={stateChart.industryData.map(({ value }) => value)}
+          seriesName="Dữ liệu Ngành"
+        />
+      ),
+    },
+    {
+      key: "2",
+      label: "Thống kê trình độ học vấn",
+      content: (
+        <GenericChart chartType="pie" title="Thống kê trình độ học vấn của nhân viên" name={names} value={values} seriesName="Trình độ học vấn" />
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <Heading
+        title={"Thống kê chi tiết " + (stateProject.project?.name || "")}
+        hasBreadcrumb
+        buttons={[
+          {
+            type: "secondary",
+            text: "Hủy",
+            icon: <IoClose className="text-[18px]" />,
+            onClick: () => {
+              navigate("/project");
             },
-            error: {
-                message: stateCompare.message,
-            },
-        },
-    });
-
-    useEffect(() => {
-        dispatchChart(projectByIndustry({}));
-        dispatchChart(projectByFundingsource({}));
-        dispatchProject(getListProject())
-            .then(unwrapResult)
-            .then((result) => {
-                const fields = result.data;
-                const formattedData = formatTreeData(fields);
-                setTreeData(formattedData);
-            });
-    }, [dispatchProject]);
-
-    useEffect(() => {
-        if (id) {
-            dispatchProject(getProjectById(id));
-        }
-    }, [id, dispatchProject]);
-
-    useEffect(() => {
-        if (selectedProjectIds.length > 0) {
-            const requestData = {
-                project_ids: selectedProjectIds,
-            };
-            dispatchCompare(compareBarChartTotalAmount(requestData));
-        }
-    }, [selectedProjectIds, dispatchCompare]);
-
-    useEffect(() => {
-        const investorId = stateProject.project?.investor?.id;
-        if (investorId) {
-            dispatchChart(employeeEducationLevelStatisticByEnterprise(investorId));
-        }
-    }, [stateProject.project, dispatchChart]);
-
-    const handleAddToCompare = () => {
-        const newIds = treeSelectIds.map(Number); // Chuyển đổi chuỗi thành số
-        const investorId = stateProject.project?.investor?.id;
-
-        // Tạo mảng project_ids và thêm investorId nếu có
-        const updatedProjectIds = [...new Set([...selectedProjectIds, ...newIds])]; // Kết hợp và loại bỏ trùng lặp
-
-        if (investorId) {
-            updatedProjectIds.push(investorId); // Thêm investorId vào mảng
-        }
-
-        setSelectedProjectIds(updatedProjectIds);
-        console.log("Selected IDs from TreeSelect:", updatedProjectIds); // Log các ID đã chọn
-    };
-
-    const educationData = stateChart.employeeEducationLevelStatisticByEnterprise || {};
-    const names = Object.keys(educationData);
-    const values = Object.values(educationData).map(value => Number(value));
-
-    const formatTreeData = (data: any[]): { title: string; value: string; key: string; children?: any[] }[] => {
-        return data.map((item) => ({
-            title: item.name,
-            value: item.id.toString(),
-            key: item.id.toString(),
-            children: item.children ? formatTreeData(item.children) : [],
-        }));
-    };
-
-    const tabItems = [
-        {
-            key: "1",
-            label: "Thống kê dự án",
-            content: (
-                <GenericChart
-                    chartType="bar"
-                    title="Thống kê ngành"
-                    name={stateChart.industryData.map(({ name }) => name)}
-                    value={stateChart.industryData.map(({ value }) => value)}
-                    seriesName="Dữ liệu Ngành"
-                />
-            ),
-        },
-        {
-            key: "2",
-            label: "Thống kê trình độ học vấn",
-            content: (
-                <GenericChart
-                    chartType="pie"
-                    title="Thống kê trình độ học vấn của nhân viên"
-                    name={names}
-                    value={values}
-                    seriesName="Trình độ học vấn"
-                />
-            ),
-        },
-    ];
-
-    return (
-        <>
-            <Heading
-                title={"Thống kê chi tiết " + (stateProject.project?.name || "")}
-                hasBreadcrumb
-                buttons={[
-                    {
-                        type: "secondary",
-                        text: "Hủy",
-                        icon: <IoClose className="text-[18px]" />,
-                        onClick: () => {
-                            navigate("/project");
-                        },
-                    },
-                ]}
-            />
-            <FormTreeSelect
-                placeholder="So sánh với dự án..."
-                treeData={treeData}
-                width="400px"
-                multiple
-                onChange={(value) => setTreeSelectIds(value)}
-            />
-            <Button
-                type="primary"
-                text="Thêm vào so sánh"
-                onClick={handleAddToCompare}
-                isDisabled={treeSelectIds.length === 0}
-            />
-            <CustomTabs items={tabItems} />
-        </>
-    );
+          },
+        ]}
+      />
+      <FormTreeSelect placeholder="So sánh với dự án..." treeData={treeData} width="400px" multiple onChange={(value) => setTreeSelectIds(value)} />
+      <Button type="primary" text="Thêm vào so sánh" onClick={handleAddToCompare} isDisabled={treeSelectIds.length === 0} />
+      <CustomTabs items={tabItems} />
+    </>
+  );
 };
 
 export default Statistical;
