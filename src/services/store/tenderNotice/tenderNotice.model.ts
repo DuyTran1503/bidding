33fe58cd77b13ab43bbfe4562f2e620b33e3220a
Dang1901 <138227152+Dang1901@users.noreply.github.com>
2024-11-06 23:53:13 +0700

    commit test

--- src/services/store/tenderNotice/tenderNotice.model.ts
+++
@@ -1,9 +0,0 @@
-export interface ITenderNotice {
-    id: string;
-    project_id: string;
-    enterprise_id: string;
-    notice_content: string;
-    notice_date: string;
-    expiry_date: string;
-    is_active: string;
-  }
