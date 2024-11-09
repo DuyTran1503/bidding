aed9524547c85d884889cc15babdafa29a4f339e
Dang1901 <138227152+Dang1901@users.noreply.github.com>
2024-11-10 00:30:54 +0700

    interface

--- src/services/store/workProgresses/workProgresses.model.ts
+++ src/services/store/workProgresses/workProgresses.model.ts
@@ -1,3 +1,11 @@
 export interface IWorkProgresses {
-    id: string
+    id: string | number,
+    bidding_result_id: string | number,
+    name: string,
+    progress: string,
+    expense: string
+    start_date: Date,
+    end_start: Date,
+    feedback: string,
+    description: string
 }
