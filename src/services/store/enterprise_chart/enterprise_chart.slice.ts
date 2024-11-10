9c2b75e4d31558900577979159dc1dde6719abdd
DuyTran <ngocduytran000315@gmail.com>
2024-11-10 16:31:33 +0700

    update code

--- src/services/store/enterprise_chart/enterprise_chart.slice.ts
+++ src/services/store/enterprise_chart/enterprise_chart.slice.ts
@@ -2,17 +2,21 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IInitialState, IResponse } from "@/shared/utils/shared-interfaces";
 import { EFetchStatus } from "@/shared/enums/fetchStatus";
-import { IChartEnterprise } from "./enterprise_chart.model";
-import { getSalaryOfEmployees } from "./enterprise_chart.thunk";
+import { IBiddingStatistic, IChartEnterprise, IResultBiddingStatistic } from "./enterprise_chart.model";
+import { getEmployeeResultBiddingStatistic, getSalaryOfEmployees } from "./enterprise_chart.thunk";
 
 export interface IChartEnterpriseInitialState extends IInitialState {
   salaryOfEmployees: IChartEnterprise[]; // Dữ liệu biểu đồ theo ngành
+  employeeResultBiddingStatistic: IResultBiddingStatistic[];
+  projectStatistic: IBiddingStatistic[];
 }
 
 const initialState: IChartEnterpriseInitialState = {
   status: EFetchStatus.IDLE,
   message: "",
   salaryOfEmployees: [],
+  employeeResultBiddingStatistic: [],
+  projectStatistic: [],
   totalRecords: 0,
   filter: {
     size: 10,
@@ -30,6 +34,14 @@ const chartEnterpriseSlice = createSlice({
       state.status = EFetchStatus.FULFILLED;
     });
     builder.addCase(getSalaryOfEmployees.rejected, (state, action) => {
+      state.status = EFetchStatus.REJECTED;
+      state.message = (action.payload as string) || "Có lỗi xảy ra khi tải dữ liệu";
+    });
+    builder.addCase(getEmployeeResultBiddingStatistic.fulfilled, (state, { payload }: PayloadAction<IResponse<IChartEnterprise[]> | any>) => {
+      state.employeeResultBiddingStatistic = payload.data;
+      state.status = EFetchStatus.FULFILLED;
+    });
+    builder.addCase(getEmployeeResultBiddingStatistic.rejected, (state, action) => {
       state.status = EFetchStatus.REJECTED;
       state.message = (action.payload as string) || "Có lỗi xảy ra khi tải dữ liệu";
     });
