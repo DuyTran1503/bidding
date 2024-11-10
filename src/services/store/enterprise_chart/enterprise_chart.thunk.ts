d55009956fabb5e33f6778143c775bce3534f4bd
DuyTran <ngocduytran000315@gmail.com>
2024-11-10 16:31:33 +0700

    update code

--- src/services/store/enterprise_chart/enterprise_chart.thunk.ts
+++ src/services/store/enterprise_chart/enterprise_chart.thunk.ts
@@ -4,7 +4,7 @@ import { IThunkPayload } from "@/shared/utils/shared-interfaces";
 
 const prefix = "/api/admin/charts/enterprises/";
 
-export const getSalaryOfEmployees = createAsyncThunk("staff/salary_of_employees", async (payload: IThunkPayload, { rejectWithValue }) => {
+export const getSalaryOfEmployees = createAsyncThunk("enterprises/salary_of_employees", async (payload: IThunkPayload, { rejectWithValue }) => {
   try {
     const { response, data } = await client.post(prefix + "employee-salary-statistic-by-enterprise", payload);
     return response.status >= 400 ? rejectWithValue(data) : data;
@@ -12,3 +12,26 @@ export const getSalaryOfEmployees = createAsyncThunk("staff/salary_of_employees"
     return rejectWithValue(error.response.data);
   }
 });
+
+export const getEmployeeResultBiddingStatistic = createAsyncThunk(
+  "enterprises/employee-result-bidding-statistic-by-enterprise",
+  async (payload: IThunkPayload, { rejectWithValue }) => {
+    try {
+      const { response, data } = await client.post(prefix + "employee-result-bidding-statistic-by-enterprise", payload);
+      return response.status >= 400 ? rejectWithValue(data) : data;
+    } catch (error: any) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+export const getEmployeeProjectStatistic = createAsyncThunk(
+  "enterprises/employee-project-statistic-by-enterprise",
+  async (payload: IThunkPayload, { rejectWithValue }) => {
+    try {
+      const { response, data } = await client.post(prefix + "employee-project-statistic-by-enterprise", payload);
+      return response.status >= 400 ? rejectWithValue(data) : data;
+    } catch (error: any) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
