6dec917a266c97c3475a55018faca54455b6e1c1
trangtth <lequyhieu1024@gmail.com>
2024-12-12 12:02:02 +0700

    fix update banner

--- src/pages/Banners/BannerForm.tsx
+++ src/pages/Banners/BannerForm.tsx
@@ -52,7 +52,11 @@ const BannerForm = ({ visible, type, setVisible, item }: IBannerFormProps) => {
         setErrors(apiErrors);
       });
     } else if (type === EButtonTypes.UPDATE) {
-      const newData = item?.path === body.path ? (({...rest }) => rest)(body) : body;
+      const newData = item?.path === body.path ? {
+        ...body,
+        path: ""
+      } : body;
+
       dispatch(updateBanner({ body: newData, param: item?.id }));
     }
   };
