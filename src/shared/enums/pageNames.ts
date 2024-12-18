1849e8d97f789fe42feb0a8df8d528707ed2a7e8
Dangph <phamhaidang190123@gmail.com>
2024-12-18 20:50:33 +0700

    employess pageNamepageName

--- src/shared/enums/pageNames.ts
+++ src/shared/enums/pageNames.ts
@@ -32,5 +32,6 @@ export enum EPageNames {
   "evaluates" = "Đánh giá kết quả dự án",
   "attachment" = "Tài liệu đính kèm",
   "project-approval" = "Phê duyệt dự án",
-  "funding-sources" = "Nguồn tài trợ"
+  "funding-sources" = "Nguồn tài trợ",
+  "employees" = "Nhân viên"
 } 
