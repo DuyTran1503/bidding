4d71703eb0c38539b9aa14fd5e8e5380475c9cca
huanhc1509 <huanhc1509@gmail.com>
2024-12-19 16:15:56 +0700

    Fix bug client

--- src/Client/Home/components/Content/index.tsx
+++ src/Client/Home/components/Content/index.tsx
@@ -6,6 +6,7 @@ import { IChartInitialState } from "@/services/store/chart/chart.slice";
 import {
   averageProjectPurationByIndustry,
   projectByDomestic,
+  projectByFundingsource,
   projectByIndustry,
   projectByOrganizationType,
   projectBySelectionMethod,
@@ -19,7 +20,7 @@ import {
 } from "@/services/store/chart/chart.thunk";
 import { Col, Row } from "antd";
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import Overview from "./Overview";
 
 // const yearOptions = Array.from({ length: 50 }, (_, i) => new Date().getFullYear() - i).map(String);
 const Content: React.FC = () => {
@@ -33,6 +34,7 @@ const Content: React.FC = () => {
   useEffect(() => {
     const dispatchActions = [
       projectByIndustry({}),
+      projectByFundingsource({}),
       averageProjectPurationByIndustry({}),
       projectByDomestic({}),
       projectByOrganizationType({}),
@@ -87,36 +89,8 @@ const Content: React.FC = () => {
   // }
 
   return (
-    <div className="mt-6">
-      <div className="w-full">
-        <h2 className="mb-4 text-xl font-semibold">1. Tổng quan về thị trường đấu thầu</h2>
-        <Row gutter={[16, 16]}>
-          <Col xs={24} sm={24} md={24} xl={12}>
-            <h3 className="mb-4 text-lg font-semibold">Tổng giá trị trúng thầu toàn quốc</h3>
-            <ul className="list-inside list-disc">
-              <li>Thống kê tổng giá trị công bố trúng thầu của thị trường đấu thầu việt nam trong 12 tháng qua.</li>
-              <li>Thống kê đã loại trừ các gói thầu đã công bố kết quả nhưng sau đó đã bị huỷ bỏ.</li>
-            </ul>
-          </Col>
-          <Col xs={24} sm={24} md={24} xl={12}>
-            <h3 className="mb-4 text-lg font-semibold">Tổng số gói thầu</h3>
-            <p>
-              Thống kê tổng số gói thầu đã được đăng tải chào thầu công khai trên
-              <Link to={"https://muasamcong.mpi.gov.vn"} className="text-blue-500">
-                {" "}
-                https://muasamcong.mpi.gov.vn{" "}
-              </Link>
-              Trong số này, chúng tôi cũng đã phân loại chia theo:
-            </p>
-            <ul className="list-inside list-disc">
-              <li>Số gói thầu đã đóng</li>
-              <li>Số gói thầu đang mở thầu</li>
-              <li>Số gói thầu mới đăng tải trong 24h</li>
-              <li>Số gói thầu mới có cập nhật/thay đổi trạng thái trong ngày</li>
-            </ul>
-          </Col>
-        </Row>
-      </div>
+    <div className="mt-6 space-y-4">
+      <Overview/>
       <div className="w-full">
         <h2 className="mb-4 text-xl font-semibold">2. Top biểu đồ</h2>
         <Row gutter={[16, 16]}>
@@ -286,10 +260,10 @@ const Content: React.FC = () => {
 
           <Col xs={24} sm={24} md={24} xl={12}>
             <ChartSection
-              title="3.8 Thời gian trung bình của dự án theo ngành"
-              chartTitle="Thời gian trung bình của dự án theo ngành"
+              title="3.8 Thời gian thực hiện trung bình của dự án theo các ngành"
+              chartTitle="Thời gian thực hiện trung bình của dự án theo các ngành"
               data={state.durationData}
-              chartType="pie"
+              chartType="bar"
               valueType="date"
               description={[
                 "Biểu đồ này thể hiện thời gian trung bình hoàn thành các dự án trong từng ngành khác nhau, giúp nhận biết ngành nào có chu kỳ dự án dài hoặc ngắn hơn.",
