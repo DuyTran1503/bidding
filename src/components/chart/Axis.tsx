0c3fbaf2742de39a4a16376ef796a6e1e237138e
DuyTran <ngocduytran000315@gmail.com>
2024-12-21 00:24:08 +0700

    update code

--- src/components/chart/Axis.tsx
+++ src/components/chart/Axis.tsx
@@ -64,7 +64,7 @@ const AbleBarChart: React.FC<AbleBarChartProps> = ({ data, title, xAxisData }) =
       fontSize: 13,
       formatter: (params) => {
         // Dùng toán tử optional chaining và thay thế undefined bằng chuỗi rỗng
-        const truncatedName = (params.seriesName ?? "").length > 30 ? (params.seriesName ?? "").substring(0, 30) + "..." : params.seriesName ?? ""; 
+        const truncatedName = (params.seriesName ?? "").length > 30 ? (params.seriesName ?? "").substring(0, 30) + "..." : params.seriesName ?? "";
         return `${params.value}  ${truncatedName}`; // Hiển thị giá trị và tên đã cắt ngắn
       },
       rich: { name: {} },
